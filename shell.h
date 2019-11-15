161ad6344d2428f0d91faaa050c24ee3dee3c308
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-15 15:01:28 -0500

    created separate test branch

--- shell.h
+++ shell.h
@@ -1,5 +1,14 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <errno.h>
 
 #endif
