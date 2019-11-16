290fe26dfa6ed461cabaf5618e4326912f9ebff9
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-16 17:33:12 -0500

    created _strcmp.c and _strcat.c

---
+++ _strcat.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+/**
+ * _strcat - function that concatenates strings
+ * @dest: destination for concatnation
+ * @src: source of string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	for (j = 0; dest[j] != '\0'; ++j)
+	{
+
+	}
+	while (src[i] != '\0')
+	{
+		dest[j + i] = src[i];
+		++i;
+	}
+	return (dest);
+}
