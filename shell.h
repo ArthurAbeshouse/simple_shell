e1a55a3a05eaae33f24f29ff4185d7954c12b676
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-17 17:02:19 -0500

    added struct for linked lists

--- shell.h
+++ shell.h
@@ -12,9 +12,20 @@
 #include <errno.h>
 #include <limits.h>
 
+typedef struct list_s
+{
+	int n;
+	struct list_s *next;
+} list_t;
+
 char *_strcpy(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strdup(char *str);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+size_t print_list(list_t *h);
+list_t *add_node_end(list_t **head, char *str);
+int delete_nodeint_at_index(list_t **head, int index);
+void free_linked_list(list_t *head);
 
 #endif
