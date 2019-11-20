173ea08573f5ed90375b1812a9972cca3ace0494
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-19 20:46:30 -0500

    replaced 'count' with the amount of words in a string

--- token.c
+++ token.c
@@ -4,17 +4,26 @@ char **token(char *buff)
 {
         char *delim = "\n ";
         char **token = NULL;
-        size_t i = 0, count = 20;
+        size_t i = 0, j, num_o_words = 0;
 
+	for (j = 0; buff[j] != '\0'; ++j)
+	{
+		if (buff[j] != ' ' && buff[j] != '\n')
+		{
+			++num_o_words;
+			while (buff[j] != ' ' && buff[j] != '\n')
+				++j;
+		}
+	}
+	/*printf("%zu\n", num_o_words);*/
 	if (buff == NULL)
 		return (0);
-	token = malloc(sizeof(char *) * count);
+	token = malloc(sizeof(char *) * (num_o_words + 1));
 	if (token == NULL)
                 return (0);
 	token[i] = strtok(buff, delim);
 	while (token[i] != NULL)
         {
-                printf("%s\n", token[i]);
 		++i;
 		token[i] = strtok(NULL, delim);
 	}
