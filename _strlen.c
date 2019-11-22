b438ce2bb924b9fcc1e5161921748076aa61a747
ArthurAbeshouse <31039144+ArthurAbeshouse@users.noreply.github.com>
2019-11-22 17:54:41 -0500

    added strlength

---
+++ _strlen.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String
+ *
+ * Return: Length of string
+ */
+
+/*String functions here*/
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length++] != '\0')
+		;
+
+	return (length - 1);
+}
